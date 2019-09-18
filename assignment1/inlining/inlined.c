c59bf442b3c2edb08adc45049144b3d501f45ef9
trilokimodi <trilokiforeducation7@gmail.com>
2019-09-18 17:29:08 +0200

    Commented out

--- assignment1/inlining/inlined.c
+++ assignment1/inlining/inlined.c
@@ -18,6 +18,6 @@ void main()
         bs_im[i] = rand() % 100 + 1;
         cs_re[i] = as_re[i]*bs_re[i] - as_im[i]*bs_im[i];
         cs_im[i] = as_re[i]*bs_im[i] + as_im[i]*bs_re[i];
-        printf("A(%d) = %.2lf + %.2lf i\tB(%d) = %.2lf + %.2lf i\tC(%d) = %.2lf + %.2lf i\n",i,as_re[i],as_im[i],i,bs_re[i],bs_im[i],i,cs_re[i],cs_im[i]);    }
+        //printf("A(%d) = %.2lf + %.2lf i\tB(%d) = %.2lf + %.2lf i\tC(%d) = %.2lf + %.2lf i\n",i,as_re[i],as_im[i],i,bs_re[i],bs_im[i],i,cs_re[i],cs_im[i]);    }
     }
 }
